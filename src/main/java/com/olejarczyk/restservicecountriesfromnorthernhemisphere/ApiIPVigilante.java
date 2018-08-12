a527e3339fd4aead2af6c73612e306fc3bcc4cba
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2018-08-12 15:41:31 +0200

    adding files

--- src/main/java/com/olejarczyk/restservicecountriesfromnorthernhemisphere/ApiIPVigilante.java
+++ src/main/java/com/olejarczyk/restservicecountriesfromnorthernhemisphere/ApiIPVigilante.java
@@ -2,16 +2,16 @@ package com.olejarczyk.restservicecountriesfromnorthernhemisphere;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.springframework.stereotype.Repository;
+
+import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
-import java.util.stream.Collectors;
 
-@Repository
+
+@Service
 public class ApiIPVigilante implements ApiInterface {
 
     private final StringBuilder apiAdrress = new StringBuilder("https://ipvigilante.com/json/");
